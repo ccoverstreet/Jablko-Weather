ebecf7ad02ba7fde3da0f9aef8dec4caf3d300fb
coverstreet <coverst2@vols.utk.edu>
2020-09-17 16:27:52 -0400

    Added chatbot function

--- module.js
+++ module.js
@@ -32,3 +32,32 @@ module.exports.get_daily_weather = async (req, res) => {
 module.exports.get_all_weather = async (req, res) => {
 	res.json(await weather.get_all_weather());
 }
+
+// -------------------- Chatbot Functions --------------------
+
+function random_int(max) {
+	return Math.floor(Math.random() * max);
+}
+
+module.exports.chatbot_current_weather = async (req, res) => {
+	const responses = [
+		"Here's the weather.",
+		"Just look outside.",
+		"Coming up with weather puns is a breeze!",
+		"What falls but never hits the ground? The temperature."
+	];
+
+	const weather_data = await weather.get_current_weather();
+
+	const temp_in_c = weather_data.current.temp - 273.15;
+	const feels_in_c = weather_data.current.feels_like - 273.15;
+
+	var message = responses[random_int(responses.length)];
+	message += `
+Temp: ${((9 / 5 * temp_in_c) + 32).toFixed(1).toString() + " °F (" + temp_in_c.toFixed(1) + " °C)"}
+Feels: ${((9 / 5 * feels_in_c) + 32).toFixed(1).toString() + " °F (" + feels_in_c.toFixed(1) + " °C)"}
+Humidity: ${weather_data.current.humidity}%,
+Desc: ${weather_data.current.weather[0].description}`;
+
+	return message;
+}
